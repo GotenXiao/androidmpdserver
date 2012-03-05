d7f9b148f000a27cd83b2a6d76bbb1ad6ce741ae
Robin Lewis <gotenxiao@gmail.com>
2012-03-05 08:00:07 +0000

    Remove generated files from repo

--- libs/poweramp_api_lib/gen/R.java.d
+++
@@ -1,3 +0,0 @@
-/home/goten/code/android_musemote/libs/poweramp_api_lib/gen/com/maxmpz/powerampapilib/R.java \
- : /home/goten/code/android_musemote/libs/poweramp_api_lib/res/values/strings.xml \
-/home/goten/code/android_musemote/libs/poweramp_api_lib/AndroidManifest.xml \
